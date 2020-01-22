c0eda2806dc721d734c346097cbbaf5ad42cb991
prachi <prachie.naik@gmail.com>
2020-01-22 12:54:01 +0530

    adding secondary constructors in overriding class

--- app/src/test/java/com/example/firstmodule/Overriding.kt
+++ app/src/test/java/com/example/firstmodule/Overriding.kt
@@ -1,9 +1,13 @@
 package com.example.firstmodule
 
+import org.junit.Test
+
 class Overriding{
+
+    @Test
     fun main() {
 
-        var dog = MyDog()
+        var dog = MyDog("Green","breed")
 
         println(dog.color)
 
@@ -17,9 +21,14 @@ class Overriding{
         open fun eat() {
             println("Animal Eating")
         }
+        //secondary constructor
+        constructor(color: String) {
+            this.color = color
+            println("From Animal: $color")
+        }
     }
 
-    class MyDog : MyAnimal() {
+    class MyDog : MyAnimal {
 
         var bread: String = ""
 
@@ -30,8 +39,15 @@ class Overriding{
         }
 
         override fun eat() {
+            //<MyAnimal> is because what if more then one interfaces inherited
             super<MyAnimal>.eat()
             println("Dog is eating")
+        }
+
+        constructor(color: String, breed: String): super(color) {
+            this.bread = breed
+
+            println("From Dog: $color and $breed")
         }
 
     }
