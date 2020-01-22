7e4e4e1f5ef00aae8bf4fd6e0fbac958c98fa6af
prachi <prachie.naik@gmail.com>
2020-01-22 16:50:58 +0530

    HigherOrderFunc

--- app/src/test/java/com/example/firstmodule/HigherOrderFunc.kt
+++ app/src/test/java/com/example/firstmodule/HigherOrderFunc.kt
@@ -4,17 +4,32 @@ import org.junit.Test
 
 class HigherOrderFunc {
     @Test
+
     fun main()
     {
         val p = Program();
         p.addTwoNumber(7,10)
+        p.addTwoNumber(7,10,object:addTwo{
+            override fun execute(sum: Int) {
+                println(sum)
+            }
+        })
     }
     class Program()
     {
-        fun addTwoNumber(a:Int,b:Int)
+        fun addTwoNumber(a:Int,b:Int,action: addTwo)
+        {
+            val sum=a+b
+            action.execute(sum)
+        }
+        fun addTwoNumber(a:Int,b:Int)// simple
         {
             val temp:Int = a+b;
             println(temp)
         }
+
+    }
+    interface addTwo{
+        fun execute(sum:Int)
     }
 }
