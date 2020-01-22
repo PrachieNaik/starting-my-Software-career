0616c6ed49ce7f58c258f27437fc61df65b1bffb
prachi <prachie.naik@gmail.com>
2020-01-22 12:42:34 +0530

    overriding

--- app/src/test/java/com/example/firstmodule/Overriding.kt
+++ app/src/test/java/com/example/firstmodule/Overriding.kt
@@ -1,5 +1,38 @@
 package com.example.firstmodule
 
 class Overriding{
+    fun main() {
 
+        var dog = MyDog()
+
+        println(dog.color)
+
+        dog.eat()
+    }
+
+    open class MyAnimal {
+
+        open var color: String = "White"
+
+        open fun eat() {
+            println("Animal Eating")
+        }
+    }
+
+    class MyDog : MyAnimal() {
+
+        var bread: String = ""
+
+        override var color: String = "Black"
+
+        fun bark() {
+            println("Bark")
+        }
+
+        override fun eat() {
+            super<MyAnimal>.eat()
+            println("Dog is eating")
+        }
+
+    }
 }
