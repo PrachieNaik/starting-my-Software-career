730b4339337b673290880c323717d8caf15f6bed
prachi <prachie.naik@gmail.com>
2020-01-23 21:56:07 +0530

    LambdaExample

--- app/src/test/java/com/example/firstmodule/LambdaExample.kt
+++ app/src/test/java/com/example/firstmodule/LambdaExample.kt
@@ -6,8 +6,11 @@ class LambdaExample {
     @Test
     fun main()
     {
-        val make: (String, String)-> String= { realName, heroName -> "${realName} is now ${heroName}" }
-        example("Tony","Iron man", make)
+//        val make: (String, String)-> String= { realName, heroName -> "${realName} is now ${heroName}" }   //method 1
+//        example("Tony","Iron man", make)
+
+
+        example("Tony","Iron man", {realName,heroName -> "${realName} is now ${heroName}"})  //method 2
 
     }
     fun example(realName:String, heroNmae:String, make: (String,String)->String)
