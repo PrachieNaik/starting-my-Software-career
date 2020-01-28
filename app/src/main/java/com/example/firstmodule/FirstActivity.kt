db3713f1ea18c98c661e28bc1664f39ba6ae44cc
prachi <prachie.naik@gmail.com>
2020-01-28 18:33:31 +0530

    Random number generation using services

--- app/src/main/java/com/example/firstmodule/FirstActivity.kt
+++ app/src/main/java/com/example/firstmodule/FirstActivity.kt
@@ -1,26 +1,46 @@
 package com.example.firstmodule
 
 import android.app.ActivityManager
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
 import android.widget.Button
-
+import android.widget.TextView
+import java.util.*
 
 
 class FirstActivity : AppCompatActivity() {
     lateinit var activityManager:ActivityManager
+
     private val TAG= FirstActivity::class.java.simpleName
     lateinit var button: Button
     lateinit var button2: Button
+    lateinit var button3: Button
+    lateinit var button4: Button
+    lateinit var button5: Button
+    lateinit var button6: Button
+    lateinit var servicePractice:ServicePractice
+    var isServiceBound:Boolean=false
+    var serviceConnection: ServiceConnection? =null
+    lateinit var textviewService:TextView
+    lateinit var serviceInternt:Intent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate")
         setContentView(R.layout.activity_first)
-
+        serviceInternt= Intent(applicationContext,ServicePractice::class.java)
         button2=findViewById(R.id.Button2)
+        button3=findViewById(R.id.Button3)
+        button4=findViewById(R.id.Button4)
+        button5=findViewById(R.id.Button5)
+        button6=findViewById(R.id.Button6)
         button=findViewById(R.id.Button1)
+        textviewService=findViewById(R.id.textviewService)
         button.setOnClickListener{
             intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
@@ -28,45 +48,93 @@ class FirstActivity : AppCompatActivity() {
 
         }
 
-        val serviceInternt= Intent(applicationContext,ServicePractice::class.java)
+
         button2.setOnClickListener{
             Log.d(TAG,"Thread"+Thread.currentThread().id)
             startService(serviceInternt)
         }
-    }
-
-    protected fun getAppTaskState(): String {
-
-        val stringBuilder = StringBuilder()
-        val totalNumberOfTasks =
-            activityManager.getRunningTasks(10).size//Returns total number of tasks - stacks
-        stringBuilder.append("\nTotal Number of Tasks: $totalNumberOfTasks\n\n")
-
-        val taskInfo =
-            activityManager.getRunningTasks(10)//returns List of RunningTaskInfo - corresponding to tasks - stacks
-
-        for (info in taskInfo) {
-            stringBuilder.append("Task Id: " + info.id + ", Number of Activities : " + info.numActivities + "\n")//Number of Activities in task - stack
-
-            // Display the root Activity of task-stack
-            stringBuilder.append(
-                "TopActivity: " + info.topActivity!!.getClassName().toString().replace(
-                    "lauchmodesdemo.youtube.codetutor.com.activitylauchmodesdemo.",
-                    ""
-                ) + "\n"
-            )
-
-            // Display the top Activity of task-stack
-            stringBuilder.append(
-                "BaseActivity:" + info.baseActivity!!.getClassName().toString().replace(
-                    "lauchmodesdemo.youtube.codetutor.com.activitylauchmodesdemo.",
-                    ""
-                ) + "\n"
-            )
-            stringBuilder.append("\n\n")
+        button3.setOnClickListener{
+            stopService(serviceInternt)
         }
-        return stringBuilder.toString()
+        button4.setOnClickListener{
+            var temp = setRandomNo()
+        }
+        button5.setOnClickListener{
+            var temp = bindService()
+        }
+        button6.setOnClickListener{
+            var temp = unbindService()
+        }
     }
+    fun bindService()
+    {
+        if(serviceConnection==null)
+        {
+            serviceConnection = object : ServiceConnection {
+                override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                    var myServiceBinder:ServicePractice.MyServiceBinder= service as ServicePractice.MyServiceBinder
+                    servicePractice=myServiceBinder.getService()
+                    isServiceBound=true
+                }
+
+                override fun onServiceDisconnected(name: ComponentName?) {
+                    isServiceBound=false
+                }
+            }
+        }
+
+        bindService(serviceInternt, serviceConnection!!, Context.BIND_AUTO_CREATE)
+
+
+
+    }
+    fun unbindService()
+    {
+        if(isServiceBound) {
+            serviceConnection?.let { unbindService(it) }
+            isServiceBound=false
+        }
+    }
+    fun setRandomNo()
+    {
+        if(isServiceBound)
+            textviewService.setText("Random number"+servicePractice.getRandomNo())
+        else
+            textviewService.setText("Service unbound")
+
+    }
+//    protected fun getAppTaskState(): String {
+//
+//        val stringBuilder = StringBuilder()
+//        val totalNumberOfTasks =
+//            activityManager.getRunningTasks(10).size//Returns total number of tasks - stacks
+//        stringBuilder.append("\nTotal Number of Tasks: $totalNumberOfTasks\n\n")
+//
+//        val taskInfo =
+//            activityManager.getRunningTasks(10)//returns List of RunningTaskInfo - corresponding to tasks - stacks
+//
+//        for (info in taskInfo) {
+//            stringBuilder.append("Task Id: " + info.id + ", Number of Activities : " + info.numActivities + "\n")//Number of Activities in task - stack
+//
+//            // Display the root Activity of task-stack
+//            stringBuilder.append(
+//                "TopActivity: " + info.topActivity!!.getClassName().toString().replace(
+//                    "lauchmodesdemo.youtube.codetutor.com.activitylauchmodesdemo.",
+//                    ""
+//                ) + "\n"
+//            )
+//
+//            // Display the top Activity of task-stack
+//            stringBuilder.append(
+//                "BaseActivity:" + info.baseActivity!!.getClassName().toString().replace(
+//                    "lauchmodesdemo.youtube.codetutor.com.activitylauchmodesdemo.",
+//                    ""
+//                ) + "\n"
+//            )
+//            stringBuilder.append("\n\n")
+//        }
+//        return stringBuilder.toString()
+//    }
 
     override fun onStart() {
         super.onStart()
--- app/src/main/java/com/example/firstmodule/ServicePractice.kt
+++ app/src/main/java/com/example/firstmodule/ServicePractice.kt
@@ -2,22 +2,64 @@ package com.example.firstmodule
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
+import kotlin.properties.Delegates
+import kotlin.random.Random
+
 private val TAG= ServicePractice::class.java.simpleName
 class ServicePractice: Service() {
+
+
+    private var MAX=100
+    private var MIN=0
+    var randomNumber =0
+    var isRandomGeneratorOn:Boolean=false
+
+     inner class MyServiceBinder : Binder() {
+        fun getService(): ServicePractice {
+            return this@ServicePractice
+        }
+    }
+    val mBinder:IBinder=MyServiceBinder()
     override fun onBind(intent: Intent?): IBinder? {
-        return null
+
+        return mBinder
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG,"Thread"+Thread.currentThread().id)
-        return super.onStartCommand(intent, flags, startId)
-    }
+        isRandomGeneratorOn=true
+        Thread(Runnable{ startRandomNumberGenerator()}).start()
 
+        return START_STICKY
+    }
+    fun startRandomNumberGenerator()
+    {
+        while(isRandomGeneratorOn)
+        {
+            Thread.sleep(1000)
+            if(isRandomGeneratorOn)
+            {
+                randomNumber= Random.nextInt(MAX)+MIN
+                Log.d(TAG,"Thread id "+Thread.currentThread().id+"Random Number "+randomNumber)
+
+            }
+        }
+    }
+    fun stopRandomNumberGenerator()
+    {
+        isRandomGeneratorOn=false
+    }
+    fun getRandomNo():Int
+    {
+        return randomNumber
+    }
     override fun onDestroy() {
         super.onDestroy()
+        stopRandomNumberGenerator()
         Log.d(TAG,"OnDestroy")
-        stopSelf()
+        //stopSelf()
     }
 }
